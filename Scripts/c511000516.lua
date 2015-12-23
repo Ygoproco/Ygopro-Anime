e5f135d8a3eff4003359bfd3b3e344d77e725584
NassX <nunosimoes_09@hotmail.com>
2015-12-23 01:03:22 +0000

    c511000516 - Number 35: Ravenous Tarantula
    added "Cannot be destroyed by battle except with "Number" monsters."

--- Scripts/c511000516.lua
+++ Scripts/c511000516.lua
@@ -15,15 +15,22 @@ function c511000516.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EFFECT_SET_DEFENCE)
 	c:RegisterEffect(e2)
+	--battle indestructable
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(511000516,0))
-	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCountLimit(1)
-	e3:SetCost(c511000516.efcost)
-	e3:SetTarget(c511000516.eftg)
-	e3:SetOperation(c511000516.efop)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e3:SetValue(c511000516.indes)
 	c:RegisterEffect(e3)
+	if not c511000516.global_check then
+		c511000516.global_check=true
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ADJUST)
+		ge2:SetCountLimit(1)
+		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+		ge2:SetOperation(c511000516.numchk)
+		Duel.RegisterEffect(ge2,0)
+	end
 end
 c511000516.xyz_number=35
 function c511000516.atktg(e,c)
@@ -37,23 +44,6 @@ function c511000516.val(e,c)
 		return Duel.GetLP(tp)-Duel.GetLP(1-tp)
 	end
 end
-function c511000516.efcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
-end
-function c511000516.eftg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	local op=Duel.SelectOption(tp,aux.Stringid(511000516,1),aux.Stringid(511000516,2))
-	e:SetLabel(op)
-end
-function c511000516.efop(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabel()==0 then
-		Duel.Recover(tp,2000,REASON_EFFECT)
-	else
-		local lp1=Duel.GetLP(tp)
-		local lp2=Duel.GetLP(1-tp)
-		Duel.SetLP(tp,lp2)
-		Duel.SetLP(1-tp,lp1)
-		Duel.Recover(tp,1000,REASON_EFFECT)
-	end
+function c511000516.indes(e,c)
+	return not c:IsSetCard(0x48)
 end
